7de6e31c74eeba8d4e0a2d55acce9e46ff6252a6
VigneshKannan1716 <114393207+VigneshKannan1716@users.noreply.github.com>
2023-04-19 23:39:54 +0530

    Update app.py

--- microservices/multiplication/app/app.py
+++ microservices/multiplication/app/app.py
@@ -1,25 +1,19 @@
 from flask import Flask
-from flask_restful import Resource, Api
+import flask.scaffold
+flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
+
+from flask_restful import Api, Resource
+class Multiplication(Resource): 
+	def get(self, num1, num2):
+		return {'result': int(num1) * int(num2)}
 
 app = Flask(__name__)
 api = Api(app)
-app.secret_key = '080_087_092_111'
+api.add_resource(Multiplication, '/<num1>/<num2>')
 
-class Multiplication(Resource):
-    def __init__(self):
-        pass
-    
-    def get(self, n1, n2):
-        try:
-            return {"error": False, "result": n1 * n2}
-        except Exception as e:
-            return {"error": True, "message": str(e)}
-
-api.add_resource(Multiplication, "/mul/<float(signed=True):n1>/<float(signed=True):n2>")
-
-if __name__ == "__main__":
-    app.run(
-        debug = True,
-        port = 5053,
-        host = "0.0.0.0"
-    )
+if __name__ =="__main__":
+	app.run(
+		debug=True,
+		port=5053,
+		host="0.0.0.0"
+	)
