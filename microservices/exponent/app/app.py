9c5d04cb2238f3edf678c7d8216aa83783d137fa
VigneshKannan1716 <114393207+VigneshKannan1716@users.noreply.github.com>
2023-04-19 23:44:51 +0530

    Create app.py

---
+++ microservices/exponent/app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import flask.scaffold
+flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
+
+from flask_restful import Api, Resource
+class Exponent(Resource): 
+	def get(self, num1, num2):
+		return {'result': int(num1) ** int(num2)}
+
+app = Flask(__name__)
+api = Api(app)
+api.add_resource(Exponent, '/<num1>/<num2>')
+
+if __name__ =="__main__":
+	app.run(
+		debug=True,
+		port=5060,
+		host="0.0.0.0"
+	)
